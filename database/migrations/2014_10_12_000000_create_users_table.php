65ce427dac35fc7ef3e1675be7a91ea227f2acd5
Nurizko Maulana <nurizko.maulana@gmail.com>
2021-01-18 07:35:28 +0700

    change

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,7 +19,7 @@ class CreateUsersTable extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->string('role')->
+            $table->enum('role', ["student","staff","faculty"])->nullable();
             $table->rememberToken();
             $table->foreignId('current_team_id')->nullable();
             $table->text('profile_photo_path')->nullable();
